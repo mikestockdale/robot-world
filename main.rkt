05640b0b454aef06bd7c796fa234680d57c845a3
mikestockdale <mikes@syterra.com>
2024-08-29 21:06:10 -0600

    bot is created on the server with an id and location

--- main.rkt
+++ main.rkt
@@ -3,16 +3,20 @@
 (module+ test (require rackunit))
 
 (struct location (x y) #:transparent)
-(struct bot (location))
+(struct bot (id location))
 
 ; server
 (define start-location (location 10 10))
-(define (add-bot!) start-location)
+(define new-id 101)
+(define (add-bot!) (bot new-id start-location))
 
 ; client
-(define (make-bot) (bot (add-bot!)))
+(define (make-bot) (add-bot!))
 
 (module+ test
   (test-case
    "bot is created at start location"
-   (check-equal? (bot-location (make-bot)) start-location)))
+   (check-equal? (bot-location (make-bot)) start-location))
+  (test-case
+   "bot is created with new id"
+   (check-equal? (bot-id (make-bot)) new-id)))
