c35ec8adf26940ab816bed3e1dff5497114d1d27
mikestockdale <mikes@syterra.com>
2024-08-30 14:03:34 -0600

    introduce methods to clarify code

--- main.rkt
+++ main.rkt
@@ -3,23 +3,31 @@
 (module+ test (require rackunit))
 
 (struct location (x y) #:transparent)
+(define (move-location start delta-x delta-y)
+  (location
+   (+ (location-x start) delta-x)
+   (+ (location-y start) delta-y)))
+
 (struct bot (id location))
 (struct world (bots))
 
 ; server
+(define (place-bot! world id location) (hash-set! (world-bots world) id location))
+(define (locate-bot world id) (hash-ref (world-bots world) id))
+
 (define start-location (location 10 10))
 (define new-id 101)
+
 (define (add-bot! world)
   (let ([new-bot (bot new-id start-location)])
-    (hash-set! (world-bots world) (bot-id new-bot) (bot-location new-bot))
+    (place-bot! world (bot-id new-bot) (bot-location new-bot))
     new-bot))
+
 (define (move-bot! world id delta-x delta-y)
   (let*
-      ([old-location (hash-ref (world-bots world) id)]
-       [new-location (location
-                      (+ (location-x old-location) delta-x)
-                      (+ (location-y old-location) delta-y))])
-    (hash-set! (world-bots world) id new-location)
+      ([old-location (locate-bot world id)]
+       [new-location (move-location old-location delta-x delta-y)])
+    (place-bot! world id new-location)
     new-location))    
 
 ; client
