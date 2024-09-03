5e46f3e639f8297c545ae50d8cc513c51ef1bfcb
mikestockdale <mikes@syterra.com>
2024-09-03 16:11:32 -0600

    use timer to perform actions in viewer

--- viewer.rkt
+++ viewer.rkt
@@ -3,30 +3,59 @@
 (require racket/gui/base)
 (require "world.rkt" "location.rkt")
 
-(let* ([world (make-world)]
-       [bot1 (add-bot! world (location 1 5))]
-       [bot2 (add-bot! world (location 1 6))]
-       [bot3 (add-bot! world (location 2 5))]
-       [frame (new frame% [label "robots"] [width 270] [height 250])]
-       [font (make-font #:face "Courier")]
-       [canvas
-        (new canvas%
-             [parent frame]
-             [paint-callback
-              (λ (canvas dc)
-                (define row 0)
-                (send dc set-font font )
-                (for ([line (draw-world world 20)])
-                  (send dc draw-text line 0 row)
-                  (set! row (+ row 11))))])])
-  (send frame show #t)
-  (send canvas refresh-now #:flush? #t)
-  
-  (for ([i 10])
-    (sleep .5)
-    (move-bot! world (bot-id bot1) direction-east)
-    (move-bot! world (bot-id bot2) direction-east)
-    (move-bot! world (bot-id bot3) direction-east)
-    (send canvas refresh-now #:flush? #t)))
+(define (go-east! world bot-id) (move-bot! world bot-id direction-east))
 
+(struct actions (world list))
+(struct action (bot-id procedure))
 
+(define (make-actions world . setups)
+  (define (make-action setup)
+    (action (bot-id (add-bot! world (first setup))) (second setup)))
+  (actions world (map make-action setups)))
+
+(define (perform-actions actions)
+  (for-each
+   (λ (action)
+     ((action-procedure action) (actions-world actions) (action-bot-id action)))
+   (actions-list actions)))
+
+(define (run-viewer actions)
+  (define run-actions #t)
+  (let* ([frame (new frame% [label "robots"] [width 500] [height 550])]
+         [font (make-font #:face "Courier")]
+         [my-canvas%
+          (class canvas%
+            (define/override (on-event event)
+              (when (send event button-down?)
+                (set! run-actions (not run-actions)))) 
+            (super-new))]
+         [canvas
+          (new my-canvas%
+               [parent frame]
+               [paint-callback
+                (λ (canvas dc)
+                  (define row 0)
+                  (send dc set-font font )
+                  (for ([line (draw-world (actions-world actions))])
+                    (send dc draw-text line 0 row)
+                    (set! row (+ row 11))))])])
+    (send frame show #t)
+    (send canvas refresh-now #:flush? #t)
+
+    (define timer
+      (new timer%
+           [interval 500]
+           [notify-callback
+            (λ () 
+              (when run-actions
+                (perform-actions actions)
+                (send canvas refresh-now #:flush? #t)))]))
+    #t))
+
+(let* ([world (make-world 50)]
+       [actions
+        (make-actions world
+                      (list (location 0 0) go-east!)
+                      (list (location 0 5) go-east!)
+                      (list (location 1 4) go-east!))])
+  (run-viewer actions))
