612044110d5b10bfa8734b2c930bebe5d8830fac
mikestockdale <mikes@syterra.com>
2024-09-03 14:55:16 -0600

    check for valid locations

--- location.rkt
+++ location.rkt
@@ -1,21 +1,30 @@
 #lang racket
 
-(provide (struct-out location) move-location
+(provide (struct-out location) move-location is-valid-location?
          direction-north direction-south direction-east direction-west)
 
 (module+ test (require rackunit))
 
 (struct location (x y) #:transparent)
 
-(define direction-north (location 0 1))
-(define direction-south (location 0 -1))
-(define direction-east (location 1 0))
-(define direction-west (location -1 0))
+(define direction-north 0)
+(define direction-east 1)
+(define direction-south 2)
+(define direction-west 3)
 
-(define (move-location start offset)
-  (location
-   (+ (location-x start) (location-x offset))
-   (+ (location-y start) (location-y offset))))
+(define movement (vector (location 0 1) (location 1 0) (location 0 -1) (location -1 0)))
+
+(define (move-location start direction)
+  (let ([offset (vector-ref movement direction)])
+    (location
+     (+ (location-x start) (location-x offset))
+     (+ (location-y start) (location-y offset)))))
+
+(define (is-valid-location? location size)
+  (and (>= (location-x location) 0)
+       (>= (location-y location) 0)
+       (< (location-x location) size)
+       (< (location-y location) size)))
 
 (module+ test
   (test-case
@@ -23,4 +32,13 @@
    (check-equal? (move-location (location 5 6) direction-north) (location 5 7))
    (check-equal? (move-location (location 5 6) direction-south) (location 5 5))
    (check-equal? (move-location (location 5 6) direction-east) (location 6 6))
-   (check-equal? (move-location (location 5 6) direction-west) (location 4 6))))
+   (check-equal? (move-location (location 5 6) direction-west) (location 4 6)))
+
+  (test-case
+   "valid locations"
+   (check-true (is-valid-location? (location 0 0) 1))
+   (check-true (is-valid-location? (location 9 9) 10))
+   (check-false (is-valid-location? (location 0 -1) 1))
+   (check-false (is-valid-location? (location -1 0) 1))
+   (check-false (is-valid-location? (location 10 9) 10))
+   (check-false (is-valid-location? (location 9 10) 10))))
--- world.rkt
+++ world.rkt
@@ -6,32 +6,37 @@
 (module+ test (require rackunit))
 
 (struct bot (id location))
-(struct world ([next-id #:mutable] bots))
+(struct world (size [next-id #:mutable] bots))
 
-(define (make-world) (world 101 (make-hash)))
+(define (make-world size) (world size 101 (make-hash)))
 
 (define (place-bot! world id location) (hash-set! (world-bots world) id location))
 (define (locate-bot world id) (hash-ref (world-bots world) id))
 
 (define (add-bot! world location)
-  (let ([new-bot (bot (world-next-id world) location)])
-    (place-bot! world (bot-id new-bot) (bot-location new-bot))
-    (set-world-next-id! world (+ 1 (world-next-id world)))
-    new-bot))
+  (if (is-valid-location? location (world-size world))
+      (let ([new-bot (bot (world-next-id world) location)])
+        (place-bot! world (bot-id new-bot) (bot-location new-bot))
+        (set-world-next-id! world (+ 1 (world-next-id world)))
+        new-bot)
+      #f))
 
 (define (move-bot! world id direction)
   (let*
       ([old-location (locate-bot world id)]
        [new-location (move-location old-location direction)])
-    (place-bot! world id new-location)
-    new-location))
+    (if (is-valid-location? new-location (world-size world))
+        (begin (place-bot! world id new-location)
+               new-location)
+        old-location)))
 
-(define (draw-world world size)
-  (let* ([lines (for/vector ([_ size]) (make-string size #\space))])
+(define (draw-world world)
+  (let* ([size (world-size world)]
+         [lines (for/vector ([_ size]) (make-string size #\space))])
     (define (draw-bot id location)
       (string-set!
-       (vector-ref lines (- size (location-y location)))
-       (- (location-x location) 1)
+       (vector-ref lines (- size 1 (location-y location)))
+       (location-x location)
        #\O))
     (hash-for-each (world-bots world) draw-bot)
     lines))
@@ -40,26 +45,41 @@
   (test-case
    "bot is created at requested location"
    (let* ([somewhere (location 1 2)]
-          [new-bot (add-bot! (make-world) somewhere)])
+          [new-bot (add-bot! (make-world 10) somewhere)])
      (check-equal? (bot-location new-bot) somewhere)))
+
+  (test-case
+   "bot is not created at invalid location"
+   (let* ([somewhere (location -1 2)]
+          [new-bot (add-bot! (make-world 10) somewhere)])
+     (check-false new-bot)))
+
   (test-case
    "bot is created with new id"
-   (let* ([world (make-world)]
+   (let* ([world (make-world 10)]
           [first-bot (add-bot! world (location 3 4))]
           [second-bot (add-bot! world (location 5 6))]          )
      (check-not-equal? (bot-id first-bot) (bot-id second-bot))))
+
   (test-case
    "move bot changes location"
-   (let* ([world (make-world)]
+   (let* ([world (make-world 10)]
           [new-bot (add-bot! world (location 5 6))])
      (check-equal? (move-bot! world (bot-id new-bot) direction-north) (location 5 7))
      (check-equal? (move-bot! world (bot-id new-bot) direction-east) (location 6 7))
      (check-equal? (move-bot! world (bot-id new-bot) direction-south) (location 6 6))
      (check-equal? (move-bot! world (bot-id new-bot) direction-west) (location 5 6))))
+
+  (test-case
+   "invalid move leaves bot location unchanged"
+   (let* ([world (make-world 10)]
+          [new-bot (add-bot! world (location 9 9))])
+     (check-equal? (move-bot! world (bot-id new-bot) direction-north) (location 9 9)))) 
+
   (test-case
    "world is drawn as strings"
-   (let ([world (make-world)])
-     (add-bot! world (location 1 3))
-     (add-bot! world (location 2 2))
-     (add-bot! world (location 3 2))
-     (check-equal? (draw-world world 3) #("O  " " OO" "   "))))) 
+   (let ([world (make-world 3)])
+     (add-bot! world (location 0 2))
+     (add-bot! world (location 1 1))
+     (add-bot! world (location 2 1))
+     (check-equal? (draw-world world) #("O  " " OO" "   "))))) 
