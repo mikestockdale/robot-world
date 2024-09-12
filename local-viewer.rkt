9e3df874fd5b04e5d38c3d0d9e1f6ad526981426
mikestockdale <mikes@syterra.com>
2024-09-11 19:39:15 -0600

    separate domain and gui code

---
+++ local-viewer.rkt
@@ -0,0 +1,30 @@
+#lang racket
+
+(require racket/gui/base)
+(require "actions.rkt" "direction.rkt" "entity.rkt" "server.rkt"
+         "world.rkt" "location.rkt" "viewer.rkt" "wandering.rkt")
+
+(let* ([world (make-world 50)]
+       [server (connect-server world)]
+       [to-do
+        (actions server
+                 (list
+                  (make-wandering server (location 20 20) direction-east)
+                  (make-wandering server (location 30 30) direction-west)))])
+  (add-entity! world type-block (location 25 25))
+  (add-entity! world type-block (location 35 25))
+  (add-entity! world type-block (location 25 35))
+  (add-entity! world type-block (location 45 25))
+  (add-entity! world type-block (location 25 45))
+  (add-entity! world type-block (location 15 25))
+  (add-entity! world type-block (location 25 15))
+  (add-entity! world type-block (location 5 25))
+  (add-entity! world type-block (location 25 5))
+
+  (define (draw-procedure draw-entity)
+    (draw-entities world draw-entity))
+
+  (define (do-actions)
+    (set! to-do (perform-actions to-do)))
+  
+  (run-viewer draw-procedure do-actions))
--- viewer.rkt
+++ viewer.rkt
@@ -1,12 +1,10 @@
 #lang racket
 
+(provide run-viewer)
 (require racket/gui/base)
-(require "actions.rkt" "direction.rkt" "entity.rkt" "server.rkt"
-         "world.rkt" "location.rkt" "wandering.rkt")
 
-(define (run-viewer world actions)
+(define (run-viewer draw-procedure timer-procedure)
   (define run-actions #t)
-  (define to-do actions)
   (let* ([frame (new frame% [label "robots"] [width 500] [height 555])]
          [font (make-font #:face "Courier")]
          [my-canvas%
@@ -23,7 +21,7 @@
                   (send dc set-font font )
                   (define (draw-entity symbol x y)
                     (send dc draw-text (string symbol) (* 10 x) (* 11 y)))
-                  (draw-entities world draw-entity))])])
+                  (draw-procedure draw-entity))])])
     (send frame show #t)
     (send canvas refresh-now #:flush? #t)
 
@@ -33,24 +31,6 @@
            [notify-callback
             (λ () 
               (when run-actions
-                (set! to-do (perform-actions to-do))
+                (timer-procedure)
                 (send canvas refresh-now #:flush? #t)))]))
     #t))
-
-(let* ([world (make-world 50)]
-       [server (connect-server world)]
-       [actions
-        (actions server
-                 (list
-                  (make-wandering server (location 20 20) direction-east)
-                  (make-wandering server (location 30 30) direction-west)))])
-  (add-entity! world type-block (location 25 25))
-  (add-entity! world type-block (location 35 25))
-  (add-entity! world type-block (location 25 35))
-  (add-entity! world type-block (location 45 25))
-  (add-entity! world type-block (location 25 45))
-  (add-entity! world type-block (location 15 25))
-  (add-entity! world type-block (location 25 15))
-  (add-entity! world type-block (location 5 25))
-  (add-entity! world type-block (location 25 5))
-  (run-viewer world actions))
