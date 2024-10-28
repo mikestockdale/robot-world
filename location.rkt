bc535d86c36074e360e12766cfdda1dd8389f7a2
mikestockdale <mikes@syterra.com>
2024-10-27 20:37:23 -0600

    documenting location

--- location.rkt
+++ location.rkt
@@ -4,24 +4,34 @@
 
 (struct location (x y) #:prefab)
 
-(define (difference field a b)
-  (abs (- (field a) (field b))))
+(define (offset field a b) (- (field a) (field b)))
+
+(define (location-offset to from)
+  (values (offset location-x to from)
+          (offset location-y to from)))
+
+(define (difference field a b) (abs (offset field a b))) 
 
 (define (adjacent? a b)
   (= 1 (+ (difference location-x a b)
           (difference location-y a b))))
 
 (define (nearby? a b)
-  (= (max (difference location-x a b)
-          (difference location-y a b))
-     1))
-
-(define (location-offset from to)
-  (values (- (location-x from) (location-x to))
-          (- (location-y from) (location-y to))))
+  (= 1 (max (difference location-x a b)
+            (difference location-y a b))))
 
 (module+ test
   (require rackunit)
+
+  (test-case
+   "location offset"
+   (define (check-offset to from expected-x expected-y) 
+     (let-values ([(delta-x delta-y) (location-offset to from)])
+       (check-equal? delta-x expected-x "x")
+       (check-equal? delta-y expected-y "y")))
+   
+   (check-offset (location 3 6) (location 1 2) 2 4)
+   (check-offset (location 1 2) (location 4 3) -3 -1))
   
   (test-case
    "adjacent locations"
--- show-demo.rkt
+++ show-demo.rkt
@@ -4,11 +4,22 @@
 
 (define (timer) #f)
 
-(define (draw draw-entity)
+(define (adjacent draw-entity)
   (draw-entity (entity-symbol (make-entity #f type-bot #f)) 2 2)
-  (draw-entity (entity-symbol (make-entity #f type-block #f)) 1 2 #:color "green")
-  (draw-entity (entity-symbol (make-entity #f type-block #f)) 3 2 #:color "green")
-  (draw-entity (entity-symbol (make-entity #f type-block #f)) 2 1 #:color "green")
-  (draw-entity (entity-symbol (make-entity #f type-block #f)) 2 3 #:color "green"))
+  (draw-entity (entity-symbol (make-entity #f type-block #f)) 1 2 #:color "light gray")
+  (draw-entity (entity-symbol (make-entity #f type-block #f)) 3 2 #:color "light gray")
+  (draw-entity (entity-symbol (make-entity #f type-block #f)) 2 1 #:color "light gray")
+  (draw-entity (entity-symbol (make-entity #f type-block #f)) 2 3 #:color "light gray"))
 
-(run-viewer "test" draw timer #:size 5 #:style '(no-caption))
+(define (nearby draw-entity)
+  (draw-entity (entity-symbol (make-entity #f type-bot #f)) 2 2)
+  (draw-entity (entity-symbol (make-entity #f type-block #f)) 1 1 #:color "light gray")
+  (draw-entity (entity-symbol (make-entity #f type-block #f)) 1 2 #:color "light gray")
+  (draw-entity (entity-symbol (make-entity #f type-block #f)) 1 3 #:color "light gray")
+  (draw-entity (entity-symbol (make-entity #f type-block #f)) 2 1 #:color "light gray")
+  (draw-entity (entity-symbol (make-entity #f type-block #f)) 2 3 #:color "light gray")
+  (draw-entity (entity-symbol (make-entity #f type-block #f)) 3 1 #:color "light gray")
+  (draw-entity (entity-symbol (make-entity #f type-block #f)) 3 2 #:color "light gray")
+  (draw-entity (entity-symbol (make-entity #f type-block #f)) 3 3 #:color "light gray"))
+
+(run-viewer "test" nearby timer #:size 5 #:style '(no-caption))
