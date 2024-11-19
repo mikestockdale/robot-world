f654ac8efb0f669fcdb7ac32aa4753cedfdeac42
mikestockdale <mikes@syterra.com>
2024-11-18 21:48:00 -0700

    document connection

--- client/connection.rkt
+++ client/connection.rkt
@@ -2,9 +2,11 @@
 
 (provide connect-remote)
 
-(define (connect-remote host port) (remote-call host port))
+;@title{Connection}
+;@margin-note{Source code at @hyperlink["https://github.com/mikestockdale/robot-world/blob/main/client/connection.rkt" "connection.rkt"]}
+;A connection is a function that sends a request to the server and receives a reply.
 
-(define (remote-call host port)
+(define (connect-remote host port)
   (let-values ([(in out) (tcp-connect host port)])
     (file-stream-buffer-mode in 'none)
     (λ (request)
