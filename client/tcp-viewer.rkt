70e9cdb88cdaec9eaaff55cf454684de38757cd8
mikestockdale <mikes@syterra.com>
2024-11-28 09:47:57 -0700

    document tcp viewer

--- client/tcp-viewer.rkt
+++ client/tcp-viewer.rkt
@@ -1,6 +1,10 @@
 #lang racket
 
-(require "shared.rkt" "client/connection.rkt" "client/viewer.rkt")
+(require "shared.rkt" "connection.rkt" "viewer.rkt")
+
+;@title{TCP Viewer}
+;@margin-note{Source code at @hyperlink["https://github.com/mikestockdale/robot-world/blob/main/client/tcp-viewer.rkt" "tcp-viewer.rkt"]}
+;The TCP viewer connects to a TCP server and opens a viewer window..
 
 (define (do-actions) #t )
   
